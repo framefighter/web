22a008b4b1df02075886d95b816fe2f8e74cf379
Leonardo Zaninelli <zaninelli@sim.tu-darmstadt.de>
2021-03-12 04:05:28 +0100

    init

---
+++ web/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from "./images/place.png";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={logo}
+          className="App-logo" 
+          alt="logo"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
